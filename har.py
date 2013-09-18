fa2cbcaf17fe739515ba4402fdd54c3f6600a2c0
Toby M <toby.moss@me.com>
2013-09-18 22:59:48 +0100

    Round 1

---
+++ har.py
@@ -0,0 +1,58 @@
+#Import the library
+
+from midiutil.MidiFile import MIDIFile
+import random
+
+# Create the MIDIFile Object with 1 track
+MyMIDI = MIDIFile(1)
+
+# Tracks are numbered from zero. Times are measured in beats.
+
+track = 0   
+time = 0
+
+# Add track name and tempo.
+MyMIDI.addTrackName(track,time,"Sample Track")
+MyMIDI.addTempo(track,time,120)
+
+# Add a note. addNote expects the following information:
+track = 0
+channel = 0
+duration = 1
+volume = 100
+
+def note(pitch, time):
+	MyMIDI.addNote(track,channel,pitch,time,duration,volume)
+
+def arpeggio(root, inversion, minor):
+	if inversion == 0:
+		chord = [root, root+4-minor, root+7]
+	if inversion == 1:
+		chord = [root+4-minor, root+7, root+12]
+	if inversion == 2:
+		chord = [root+7, root+12, root+16-minor]
+			
+	global time 
+	# Now add the note.
+	note(chord[0], time+0)
+	note(chord[1], time+1)
+	note(chord[2], time+2)
+	note(chord[1], time+3) 
+	time +=4
+
+def structure(key):
+	second = [2, 3, 4, 5, 6]
+	third = [6, 8]
+	forth = 8
+	
+	arpeggio(key, 0, random.randint(0,1))
+	arpeggio(key+second[random.randint(0, 4)], 1, 0)
+	arpeggio(key+third[random.randint(0,1)], random.randint(0,2), 1)
+	arpeggio(key+forth, 1, random.randint(0,1))
+
+structure(random.randint(40, 70))
+
+# And write it to disk.
+binfile = open("output.mid", 'wb')
+MyMIDI.writeFile(binfile)
+binfile.close()
